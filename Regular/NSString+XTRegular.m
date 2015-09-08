51d707d4b237b5348421d4909a035655adb796db
zgs225 <zgs225@gmail.com>
2015-09-08 15:12:01 +0800

    添加项目文件

---
+++ Regular/NSString+XTRegular.m
@@ -0,0 +1,116 @@
+//
+//  NSString+XTRegular.m
+//  XiaoTu
+//
+//  Created by 何振东 on 15/7/7.
+//  Copyright © 2015年 LZA. All rights reserved.
+//
+
+#import "NSString+XTRegular.h"
+
+@implementation NSString (XTRegular)
+
+- (BOOL)isValidateEmail
+{
+    NSString *regex = @"\\b([a-zA-Z0-9%_.+\\-]+)@([a-zA-Z0-9.\\-]+?\\.[a-zA-Z]{2,6})\\b";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [predicate evaluateWithObject:self];
+}
+
+- (BOOL)isNumber
+{
+    NSString *regex = @"^[0-9]*$";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [predicate evaluateWithObject:self];
+}
+
+- (BOOL)isEnglishWords
+{
+    NSString *regex = @"^[A-Za-z]+$";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [predicate evaluateWithObject:self];
+}
+
+//字母数字下划线，6－16位
+- (BOOL)isValidatePassword
+{
+    //    NSString *regex = @"^[\\w\\d_]{6,16}$";
+    NSString *regex = @"[0-9A-Za-z.@-_]{6,16}";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [predicate evaluateWithObject:self];
+}
+
+- (BOOL)isChineseWords
+{
+    NSString *regex = @"^[\u4e00-\u9fa5],{0,}$";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [predicate evaluateWithObject:self];
+}
+
+- (BOOL)isInternetUrl
+{
+    NSString *regex = @"^http://([\\w-]+\\.)+[\\w-]+(/[\\w-./?%&=]*)?$ ；^[a-zA-z]+://(w+(-w+)*)(.(w+(-w+)*))*(?S*)?$";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [predicate evaluateWithObject:self];
+}
+
+- (BOOL)isPhoneNumber
+{
+    NSString *regex = @"^(\(\\d{3,4}\\)|\\d{3,4}-)?\\d{7,8}$";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [predicate evaluateWithObject:self];
+}
+
+- (BOOL)isElevenDigitNum
+{
+    NSString *regex = @"^[0-9]*$";
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    BOOL result = [predicate evaluateWithObject:self];
+    
+    if (result && self.length == 11)
+        return YES;
+    
+    return NO;
+}
+
+-(BOOL)isMobilePhoneNum{
+    
+    NSString * MOBILE = @"^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$";
+    
+    NSString * CM = @"^1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$";
+    
+    NSString * CU = @"^1(3[0-2]|5[256]|8[56])\\d{8}$";
+    
+    NSString * CT = @"^1((33|53|8[09])[0-9]|349)\\d{7}$";
+    
+    // NSString * PHS = @"^0(10|2[0-5789]|\\d{3})\\d{7,8}$";
+    
+    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
+    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
+    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
+    BOOL res1 = [regextestmobile evaluateWithObject:self];
+    BOOL res2 = [regextestcm evaluateWithObject:self];
+    BOOL res3 = [regextestcu evaluateWithObject:self];
+    BOOL res4 = [regextestct evaluateWithObject:self];
+    
+    if (res1 || res2 || res3 || res4 )
+    {
+        return YES;
+    }
+    else
+    {
+        return NO;
+    }
+    
+}
+
+- (BOOL)isIdentifyCardNumber
+{
+    NSString *regex = @"^\\d{15}|\\d{}18$";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [predicate evaluateWithObject:self];
+}
+
+@end
---
+++ XTRegular.podspec
@@ -0,0 +1,13 @@
+Pod::Spec.new do |s|
+
+  s.name         = "XTRegular"
+  s.version      = "1.0.0"
+  s.summary      = "常用正则模块"
+  s.homepage     = "https://coding.net/u/xiaotujieshu/p/XTRegular_iOS"
+  s.license      = { :type => "MIT", :file => "LICENSE" }
+  s.author       = { "hezhendong" => "wskrdfeai@gmail.com" }
+  s.platform     = :ios, "7.0"
+  s.source       = { :git => "https://git.coding.net/xiaotujieshu/XTRegular_iOS.git", :tag => "1.0.0" }
+  s.source_files = "Regular/*"
+
+end
